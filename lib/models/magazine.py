bd656972c287e0ce29a036735c4ee68badd11f52
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-25 18:35:33 +0300

    added magazine.py

---
+++ lib/models/magazine.py
@@ -0,0 +1,23 @@
+from db.connection import get_connection
+
+class Magazine:
+    def __init__(self, id=None, name=None, category=None):
+        self.id = id
+        self.name = name
+        self.category = category
+
+    def contributors(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT DISTINCT authors.* FROM authors
+            JOIN articles ON authors.id = articles.author_id
+            WHERE articles.magazine_id = ?
+        """, (self.id,))
+        return cursor.fetchall()
+
+    def article_titles(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT title FROM articles WHERE magazine_id=?", (self.id,))
+        return [row['title'] for row in cursor.fetchall()]
