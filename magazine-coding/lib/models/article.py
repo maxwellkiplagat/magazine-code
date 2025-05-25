2cbb9328ed1df5281caf5b566a3d3098d08155d5
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-26 00:21:12 +0300

    updated the article to add diffrent feartures such as finding by: magazine,author,title and id

--- magazine-coding/lib/models/article.py
+++ magazine-coding/lib/models/article.py
@@ -1,4 +1,4 @@
-from db.connection import get_connection
+from lib.db.connection import get_connection
 
 class Article:
     def __init__(self, id=None, title=None, author_id=None, magazine_id=None):
@@ -11,15 +11,41 @@ class Article:
         conn = get_connection()
         cursor = conn.cursor()
         if self.id:
-            cursor.execute("""
-                UPDATE articles SET title=?, author_id=?, magazine_id=?
-                WHERE id=?
-            """, (self.title, self.author_id, self.magazine_id, self.id))
+            cursor.execute("UPDATE articles SET title=?, author_id=?, magazine_id=? WHERE id=?", (self.title, self.author_id, self.magazine_id, self.id))
         else:
-            cursor.execute("""
-                INSERT INTO articles (title, author_id, magazine_id)
-                VALUES (?, ?, ?) RETURNING id
-            """, (self.title, self.author_id, self.magazine_id))
-            self.id = cursor.fetchone()[0]
+            cursor.execute("INSERT INTO articles (title, author_id, magazine_id) VALUES (?, ?, ?)", (self.title, self.author_id, self.magazine_id))
+            self.id = cursor.lastrowid
         conn.commit()
         return self
+
+    @classmethod
+    def find_by_id(cls, id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE id=?", (id,))
+        row = cursor.fetchone()
+        return cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) if row else None
+
+    @classmethod
+    def find_by_title(cls, title):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE title=?", (title,))
+        rows = cursor.fetchall()
+        return [cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) for row in rows]
+
+    @classmethod
+    def find_by_author(cls, author_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE author_id=?", (author_id,))
+        rows = cursor.fetchall()
+        return [cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) for row in rows]
+
+    @classmethod
+    def find_by_magazine(cls, magazine_id):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM articles WHERE magazine_id=?", (magazine_id,))
+        rows = cursor.fetchall()
+        return [cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) for row in rows]
