5a2501a4a81eab1fbac9ddf3d85a46fee54a0e36
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-26 00:57:28 +0300

    updated the article to add diffrent feartures such as finding by: magazine,author,title and id

--- magazine-coding/lib/models/article.py
+++ magazine-coding/lib/models/article.py
@@ -6,7 +6,7 @@ class Article:
         self.title = title
         self.author_id = author_id
         self.magazine_id = magazine_id
-
+#----------method for saving---------------
     def save(self):
         conn = get_connection()
         cursor = conn.cursor()
@@ -17,7 +17,7 @@ class Article:
             self.id = cursor.lastrowid
         conn.commit()
         return self
-
+#-----------method for finding by id ------------
     @classmethod
     def find_by_id(cls, id):
         conn = get_connection()
@@ -25,7 +25,7 @@ class Article:
         cursor.execute("SELECT * FROM articles WHERE id=?", (id,))
         row = cursor.fetchone()
         return cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) if row else None
-
+#-----------method for finding by title ------------
     @classmethod
     def find_by_title(cls, title):
         conn = get_connection()
@@ -33,7 +33,7 @@ class Article:
         cursor.execute("SELECT * FROM articles WHERE title=?", (title,))
         rows = cursor.fetchall()
         return [cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) for row in rows]
-
+#-----------method for finding by author ------------
     @classmethod
     def find_by_author(cls, author_id):
         conn = get_connection()
@@ -41,7 +41,7 @@ class Article:
         cursor.execute("SELECT * FROM articles WHERE author_id=?", (author_id,))
         rows = cursor.fetchall()
         return [cls(id=row["id"], title=row["title"], author_id=row["author_id"], magazine_id=row["magazine_id"]) for row in rows]
-
+#-----------method for finding by magazine -------------
     @classmethod
     def find_by_magazine(cls, magazine_id):
         conn = get_connection()
