e929827bddc086ab28f25387aefacb8ae3c5779b
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-26 00:34:55 +0300

    added init file

--- magazine-coding/scripts/__init__.py
+++ magazine-coding/scripts/__init__.py
@@ -1,3 +1,3 @@
-from models.author import Author
-from models.article import Article
-from models.magazine import Magazine
+from lib.models.author import Author
+from lib.models.article import Article
+from lib.models.magazine import Magazine
